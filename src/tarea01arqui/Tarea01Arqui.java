6c4a4800c8a65b2249de1ff5a2f66802b05d2b3c
Luis <lgvv06@gmail.com>
2014-09-17 11:44:40 -0600

    Lee archivos
    Ya lee archivos desde el main

---
+++ src/tarea01arqui/Tarea01Arqui.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tarea01arqui;
+
+/**
+ *
+ * @author b06789
+ */
+public class Tarea01Arqui {
+    
+    public int memInstrucciones[];
+    public int memDatos[];
+    
+    public static void main(String args[]){
+        
+        
+        
+        
+    }
+    
+    
+}
