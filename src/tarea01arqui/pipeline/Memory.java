70537e5d2f7473c6c14d246d3584b1628b6ab7c8
MarcoCh <mandres0112@gmail.com>
2014-09-17 23:39:56 -0600

    Update Memory.java

--- src/tarea01arqui/pipeline/Memory.java
+++ src/tarea01arqui/pipeline/Memory.java
@@ -1,15 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Programa hecho por:
+ * Luis Velasquez - B06789
+ * Marco Chacon - B11750
+ * Ricardo Torres - B06324
  */
 
-package tarea01arqui.pipeline;
+package tareaprog2.pipeline;
 
-/**
- *
- * @author b06789
- */
 public class Memory {
+    
+    public Memory(){
+        
+        
+        
+    }
     
 }
