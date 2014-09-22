b010ae6ec1384a5876694f2390c1faa9305f9b8b
MarcoCh <mandres0112@gmail.com>
2014-09-22 17:54:18 -0600

    Update Execute.java

--- src/tareaprog2/pipeline/Execute.java
+++ src/tareaprog2/pipeline/Execute.java
@@ -7,11 +7,45 @@
 
 package tareaprog2.pipeline;
 
-public class Execute {
+import tareaprog2.pipelineRegister;
+
+public class Execute extends Thread{
     
-    public Execute(){
+    pipelineRegister ex_mem;
+    pipelineRegister id_ex;
+    
+    public Execute(pipelineRegister ex_mem, pipelineRegister id_ex){
+         this.ex_mem = ex_mem;
+         this.id_ex = id_ex;
         
         
+    }
+    @Override
+    public void run(){
+        
+        ex_mem.setIR(id_ex.getIR());
+        
+        ex_mem.setRegister(1, id_ex.getRegister(1));
+        
+        switch(id_ex.getIR()[0]){
+            case 32:
+                ex_mem.setRegister(0, (id_ex.getRegister(0) + id_ex.getRegister(1)));
+                break;
+            case 34:
+                ex_mem.setRegister(0, (id_ex.getRegister(0) - id_ex.getRegister(1)));
+                break;
+            case 12:
+                ex_mem.setRegister(0, (id_ex.getRegister(0) * id_ex.getRegister(1)));
+                break;
+            case 14:
+                ex_mem.setRegister(0, (id_ex.getRegister(0) / id_ex.getRegister(1)));
+                break;
+            case 8:
+                ex_mem.setRegister(0, (id_ex.getRegister(0) + id_ex.getRegister(2)));
+                break;
+            case 35:
+                
+        }
         
     }
     
